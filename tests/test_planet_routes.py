9d4a6e281cb01e4dcbb841d78d1864c0a65ba194
liyan <liyangeneral@gmail.com>
2025-10-30 11:34:56 -0700

    wave 6

---
+++ tests/test_planet_routes.py
@@ -0,0 +1,44 @@
+
+def test_get_all_planets_with_no_records(client):
+    # Act
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_one_planet(client, one_planet):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": 1,
+        "name": "planet_x",
+        "description": "xxxx",
+        "moon": "x moons"
+    }
+
+
+
+def test_create_one_planet(client):
+    # Act
+    response = client.post("/planets", json={
+        "name": "planet_y",
+        "description": "yyyy",
+        "moon": "y moons"
+    })
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 1,
+        "name": "planet_y",
+        "description": "yyyy",
+        "moon": "y moons"
+    }
