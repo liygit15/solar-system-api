42bcf896c65e366c420fd587ee0de07b133b1ba3
liyan <liyangeneral@gmail.com>
2025-11-04 14:10:55 -0800

    Co-authored-by: Anaiah <3 <Anaiahm@users.noreply.github.com> Co-authored-by: geethavs88 <geethavs88@users.noreply.github.com>

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,5 +1,9 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .moon import Moon
 
 class Planet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
